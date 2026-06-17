fdb494c0df1b5dfd8fdcb7269ba86286d277cfc8
DuyVo2005 <vlkd1001@gmail.com>
2026-06-17 08:44:57 +0700

    validate nội dung thật của file

--- ExpressJS01/src/middleware/fileValidate.js
+++ ExpressJS01/src/middleware/fileValidate.js
@@ -1,11 +1,12 @@
 const multer = require("multer");
+const { fileTypeFromBuffer } = require("file-type");
 
 const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
     limits: { fileSize: 2 * 1024 * 1024 }, // Giới hạn 2MB
     fileFilter: (req, file, fileFilterCallback) => {
-        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') { // Check sơ bộ
             fileFilterCallback(null, true);
         } else {
             fileFilterCallback(new Error('Định dạng file không hợp lệ! Chỉ chấp nhận JPEG hoặc PNG.'), false);
@@ -14,15 +15,48 @@ const upload = multer({
 });
 
 const validateAvatar = (req, res, next) => {
-    upload.single('avatar')(req, res, (err) => {
+    upload.single('avatar')(req, res, async (err) => {
         if (err instanceof multer.MulterError) {
             if (err.code === 'LIMIT_FILE_SIZE') {
                 return next(new Error("File quá lớn! Tối đa chỉ được 2MB."));
             }
             return next(new Error(err.message));
-        } else if (err) {
+        }
+        
+        if (err) {
             return next(new Error(err.message));
         }
+
+        // Không có file
+        if (!req.file) {
+            return next();
+        }
+
+        // Kiểm tra nội dung thật của file
+        const type = await fileTypeFromBuffer(req.file.buffer);
+
+        if (!type) {
+            return next(new Error("File không hợp lệ"));
+            //.txt đổi tên thành .png: fileTypeFromBuffer() 
+            // không nhận diện được loại file (file giả hoàn toàn) (type = undefined) 
+        }
+
+        if (
+            type.mime !== "image/jpeg" &&
+            type.mime !== "image/png"
+        ) {
+            return next(
+                new Error("File phải là ảnh JPEG hoặc PNG thật")
+                //vd: .gif đổi tên thành .png (vẫn là GIF thật)
+                // type = {
+                //     ext: "gif",
+                //     mime: "image/gif"
+                // }
+                // -> type tồn tại
+                // khác whitelist -> file hợp lệ nhưng không nằm trong loại cho phép
+            );
+        }
+
         next(); // Nếu file hợp lệ, cho phép đi tiếp
     });
 };
